7611ed5d035d7bae59fb1f681410410a02aba41e
Alicia Ruiz <AliciaRuTo@MacBook-Pro-de-Alicia-3.local>
2019-01-13 20:09:08 +0100

    Laboratorio 2 finalizado

--- cpe.R
+++ cpe.R
@@ -6,29 +6,56 @@
 #                                                                              #
 #******************************************************************************#
 
-# install.packages("xml2")
-# library(xml2)
-
-# compressed_cpes_url <- "https://nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-dictionary_v2.3.xml.zip"
-# cpes_filename <- "cpes.zip"
-# download.file(compressed_cpes_url, cpes_filename)
-# unzip(zipfile = cpes_filename)
-cpe.file <- "./official-cpe-dictionary_v2.3.xml"
-
-GetCPEItems <- function(cpe.raw) {
-  cpe <- NewCPEItem()
-  cpe.raw <- xml2::xml_find_all(cpe.raw)
-
-  # transform the list to data frame
-
-  # return data frame
+GetCPEFile <- function() {
+  compressed_cpes_url <- "https://nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-dictionary_v2.3.xml.zip"
+  cpes_filename <- "cpes.zip"
+  download.file(compressed_cpes_url, cpes_filename)
+  unzip(zipfile = cpes_filename)
+  cpe.file <- "./official-cpe-dictionary_v2.3.xml"
+  cpes.xml <- xml2::read_xml(cpe.file)
+  return(cpes.xml)
 }
 
-CleanCPEs <- function(cpes){
+GetCPEItems <- function() {
+  cpe.title <- xml2::xml_find_all(cpes.xml, "//*[cpe-23:cpe23-item]/*[1]")
+  cpe.name <- xml2::xml_find_all(cpes.xml, "//*[cpe-23:cpe23-item]/*/@name")
 
-  # data manipulation
+  cpe <- data.frame(title = xml2::xml_text(cpe.title),
+                     cpe.23 = xml2::xml_text(cpe.name),
+                     stringsAsFactors = F)
+  return(cpe)
+}
 
-  return(data.frame())
+CleanCPEs <- function(){
+
+  col.names <- c("cpe",
+                 "v",
+                 "part",
+                 "vendor",
+                 "product",
+                 "version",
+                 "update",
+                 "edition",
+                 "language",
+                 "sw_edition",
+                 "target_sw",
+                 "target_hw",
+                 "other")
+
+  cpe$cpe.23 <- stringr::str_replace_all(cpe$cpe.23, "\\\\:", ";")
+  cpe.clear <- tidyr::separate(data = cpe, col = cpe.23, into = col.names, sep = ":", remove = F)
+  cpe.clear$cpe <- as.factor(cpe.clear$cpe)
+  cpe.clear$v <- as.factor(cpe.clear$v)
+  cpe.clear$part <- as.factor(cpe.clear$part)
+  cpe.clear$vendor <- as.factor(cpe.clear$vendor)
+  cpe.clear$product <- as.factor(cpe.clear$product)
+  cpe.clear$language <- as.factor(cpe.clear$language)
+  cpe.clear$sw_edition <- as.factor(cpe.clear$sw_edition)
+  cpe.clear$target_sw <- as.factor(cpe.clear$target_sw)
+  cpe.clear$target_hw <- as.factor(cpe.clear$target_hw)
+  cpe.clear$other <- as.factor(cpe.clear$other)
+
+  return(cpe.clear)
 }
 
 ParseCPEData <- function(cpe.file) {
@@ -44,4 +71,14 @@ ParseCPEData <- function(cpe.file) {
 
   # return data frame
   return(df)
+}
+
+##############
+
+NewCPEItem <- function(){
+  return(data.frame(cpe.22 = character(),
+                    cpe.23 = character(),
+                    cpe.ref = character(),
+                    stringsAsFactors = FALSE)
+  )
 }
